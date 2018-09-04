3813fad835843b808b1d3bcb87a2063e236e6724
putrihuzaima <42967462+putrihuzaima@users.noreply.github.com>
2018-09-04 19:26:35 +0700

    Add files via upload

---
+++ PAM/JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+
+/**
+ *
+ * @author putrihz
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int jumlah = 5;
+
+    for (int i=0; i<jumlah; i++){
+    for(int j=0; j<=i; j++){
+      System.out.print("X");
+                }
+       System.out.print("\n");
+        }
+    System.out.print("\n");
+}
+
+    }
+    
